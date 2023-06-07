e0e5e0c692f91dca9682d2d4c6e5c57e0d4b5ff2
jian yang <1065656359@qq.com>
2023-06-07 09:34:32 +0800

    0

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import yayJpg from '../assets/yay.jpg';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>我是生产环境</h2>
+
+      <h2>Yay! Welcome to umi!</h2>
+
+      <p>
+        <img src={yayJpg} width="388" />
+      </p>
+      <p>
+        To get started, edit <code>pages/index.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+}
