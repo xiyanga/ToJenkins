bed7c427b1790b9795ea040523243eb4073396ac
jian yang <1065656359@qq.com>
2023-06-08 14:22:49 +0800

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import yayJpg from '../assets/yay.jpg';
 export default function HomePage() {
   return (
     <div>
-      <h2>我是测试环境</h2>
+      <h2>我是正式环境</h2>
 
       <h2>Yay! Welcome to umi!</h2>
 
